f9e1caa5474420773dd1aa29cabd8929f5329b65
dlabaja <dlabaja.jan@seznam.cz>
2026-05-10 22:26:32 +0200

    suffixes, failure links

--- src/aho-corasick/trie-node.ts
+++ src/aho-corasick/trie-node.ts
@@ -1,12 +1,14 @@
 export class TrieNode {
     public readonly char: char;
     public readonly next: Map<char, TrieNode>;
+    public readonly failureLinks: Map<char, TrieNode>;
     public prev: TrieNode|null;
     public readonly isEnd: boolean;
     
     constructor(char: char, prev: TrieNode|null, isEnd: boolean) {
         this.char = char;
         this.next = new Map();
+        this.failureLinks = new Map();
         this.prev = prev;
         this.isEnd = isEnd;
     }
--- src/aho-corasick/trie.ts
+++ src/aho-corasick/trie.ts
@@ -12,14 +12,110 @@ export class Trie {
         this.buildTrie(words)
     }
     
+    public search(text: string) {
+        const result: string[] = [];
+        this.searchRec(text, this.root, 0, [], result);
+        return result;
+    }
+    
+    private searchRec(text: string, node: TrieNode, index: number, ir: char[], result: string[]) {
+        if (index >= text.length) {
+            return;
+        }
+        
+        const char = text[index];
+        let nextNode = node.next.get(char);
+
+        if (node.isEnd) {
+            result.push(ir.join(""));
+            ir = [];
+        }
+        
+        if (!nextNode) { // jdu do failure linku
+            nextNode = node.failureLinks.get(char);
+            if (!nextNode) { // jdu do rootu
+                this.searchRec(text, this.root, index + 1, [], result);
+                return;
+            }
+            ir = [...this.longestSuffixInTree(this.suffixes(node))];
+            this.searchRec(text, nextNode, index + 1, ir, result);
+            return;
+        }
+
+        this.searchRec(text, nextNode, index + 1, ir, result)
+    }
+    
+    // root->C->A->T -> [CAT, AT, T, EMPTY]
+    private suffixes(node: TrieNode) {
+        const result: string[] = [];
+        result.push(EMPTY);
+        
+        let currentNode = node;
+        while (currentNode != this.root) {
+            if (!result.length) {
+                result.push(currentNode.char);
+            }
+            else {
+                result.push(result[result.length - 1] + currentNode.char);
+            }
+            currentNode = currentNode.prev!;
+        }
+        
+        return result.map(r => r.split("").reverse().join("")).reverse();
+    }
+    
+    // projde od rootu strom a checkne jestli je tam suffix
+    private containsSuffix(suffix: string, node: TrieNode, index: number): boolean {
+        if (suffix == EMPTY) {
+            return true;
+        }
+        
+        if (index == suffix.length - 1) {
+            return true;
+        }
+        
+        let next = node.next.get(suffix[index])
+        if (!next) {
+            return false;
+        }
+        
+        return this.containsSuffix(suffix, next, index + 1);
+    }
+    
+    private longestSuffixInTree(suffixes: string[]) {
+        for (const suffix of suffixes) {
+            if (this.containsSuffix(suffix, this.root, 0)) {
+                return suffix;
+            }
+        }
+        return EMPTY; // nemělo by se stát
+    }
+
+    public traverseNodes(root: TrieNode, fun: (node: TrieNode) => void) {
+        const visited = new Set<TrieNode>();
+        this.traverseNodesRec(root, visited, fun);
+    }
+
+    private traverseNodesRec(node: TrieNode, visited: Set<TrieNode>, fun: (node: TrieNode) => void) {
+        if (visited.has(node)) {
+            return
+        }
+
+        fun(node);
+        visited.add(node)
+        for (const nextNode of node.next.values()) {
+            this.traverseNodesRec(nextNode, visited, fun);
+        }
+    }
+    
     private buildTrie(words: string[]) {
         for (const word of words) {
             this.addWord(word, this.root, 0);
         }
 
-        this.traverseNodes(this.root, (node) => {
-            node.next.set(EMPTY, this.root)
-        })
+        /*this.traverseNodes(this.root, (node) => {
+            node.failureLinks.set(EMPTY, this.root)
+        })*/
     }
     
     private addWord(word: string, prevNode: TrieNode, index: number) {
@@ -36,22 +132,5 @@ export class Trie {
         }
         
         this.addWord(word, node, index + 1);
-    }
-
-    public traverseNodes(root: TrieNode, fun: (node: TrieNode) => void) {
-        const visited = new Set<TrieNode>();
-        this.traverseNodesRec(root, visited, fun);
-    }
-
-    private traverseNodesRec(node: TrieNode, visited: Set<TrieNode>, fun: (node: TrieNode) => void) {
-        if (visited.has(node)) {
-            return
-        }
-        
-        fun(node);
-        visited.add(node)
-        for (const nextNode of node.next.values()) {
-            this.traverseNodesRec(nextNode, visited, fun);
-        }
     }
 }
