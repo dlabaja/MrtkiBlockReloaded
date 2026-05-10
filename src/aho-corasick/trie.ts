a2b99e0405d1e351b4fd96b16416454b7d26db71
dlabaja <dlabaja.jan@seznam.cz>
2026-05-10 22:59:10 +0200

    teď už by asi teoreticky mělo fungovat vytváření stromu a následný hledání
    provozuji intenzivní vibecoding (= programování v rytmu písniček rádia impuls)

--- src/aho-corasick/trie.ts
+++ src/aho-corasick/trie.ts
@@ -1,3 +1,6 @@
+// k tomuhle algoritmu (aho-corasick) mě přivedl chatgpt někdy v 11 večer - napsal jsem si ho sám, ale kdybych o něm nevěděl, musel bych použít regex
+// pomocí datové struktury Trie najde jedním průchodem všechny výskyty slov v textu
+
 import {TrieNode} from "./trie-node";
 
 export const EMPTY = "";
@@ -10,6 +13,42 @@ export class Trie {
         this.root.prev = this.root;
         this.words = words;
         this.buildTrie(words)
+    }
+
+    private buildTrie(words: string[]) {
+        for (const word of words) {
+            this.addWord(word, this.root, 0);
+        }
+
+        this.traverseNodes(this.root, (node) => this.addFailureLinks(node))
+    }
+
+    private addWord(word: string, prevNode: TrieNode, index: number) {
+        if (index >= word.length) {
+            return
+        }
+
+        const char = word[index];
+        const isEnd = index == word.length - 1;
+        let node = prevNode.next.get(char);
+        if (!node) {
+            node = new TrieNode(char, prevNode, isEnd);
+            prevNode.next.set(char, node);
+        }
+
+        this.addWord(word, node, index + 1);
+    }
+    
+    private addFailureLinks(node: TrieNode) {
+        const longestSuffix = this.longestSuffixInTree(this.suffixes(node));
+        const nextNode = this.findSubstring(longestSuffix, this.root);
+        if (!nextNode) {
+            return;
+        }
+        
+        for (const [char, linkNode] of nextNode.next) {
+            node.failureLinks.set(char, linkNode);
+        }
     }
     
     public search(text: string) {
@@ -46,7 +85,7 @@ export class Trie {
     }
     
     // root->C->A->T -> [CAT, AT, T, EMPTY]
-    private suffixes(node: TrieNode) {
+    private substrings(node: TrieNode) {
         const result: string[] = [];
         result.push(EMPTY);
         
@@ -64,33 +103,50 @@ export class Trie {
         return result.map(r => r.split("").reverse().join("")).reverse();
     }
     
-    // projde od rootu strom a checkne jestli je tam suffix
-    private containsSuffix(suffix: string, node: TrieNode, index: number): boolean {
-        if (suffix == EMPTY) {
-            return true;
+    private suffixes(node: TrieNode) {
+        const substrings = this.substrings(node);
+        if (substrings.length == 1) {
+            return substrings;
+        }
+        return substrings.slice(1);
+    }
+    
+    // projde od rootu strom a vrátí nalezený substring
+    public findSubstring(substring: string, node: TrieNode) {
+        return this.findSubstringRec(substring, node, 0);
+    }
+    
+    public containsSubstring(substring: string, node: TrieNode) {
+        return !!this.findSubstring(substring, node);
+    }
+    
+    private findSubstringRec(substring: string, node: TrieNode, index: number): TrieNode|null {
+        if (substring == EMPTY) {
+            return this.root;
         }
         
-        if (index == suffix.length - 1) {
-            return true;
+        if (index == substring.length - 1) {
+            return node;
         }
         
-        let next = node.next.get(suffix[index])
+        let next = node.next.get(substring[index])
         if (!next) {
-            return false;
+            return null; // jsem uprostřed stromu i substringu a dál už nemůžu
         }
         
-        return this.containsSuffix(suffix, next, index + 1);
+        return this.findSubstringRec(substring, next, index + 1);
     }
     
     private longestSuffixInTree(suffixes: string[]) {
         for (const suffix of suffixes) {
-            if (this.containsSuffix(suffix, this.root, 0)) {
+            if (this.containsSubstring(suffix, this.root)) {
                 return suffix;
             }
         }
         return EMPTY; // nemělo by se stát
     }
 
+    // preorder - nejdřív volá fn() na node a pak jde do potomků
     public traverseNodes(root: TrieNode, fun: (node: TrieNode) => void) {
         const visited = new Set<TrieNode>();
         this.traverseNodesRec(root, visited, fun);
@@ -106,31 +162,5 @@ export class Trie {
         for (const nextNode of node.next.values()) {
             this.traverseNodesRec(nextNode, visited, fun);
         }
-    }
-    
-    private buildTrie(words: string[]) {
-        for (const word of words) {
-            this.addWord(word, this.root, 0);
-        }
-
-        /*this.traverseNodes(this.root, (node) => {
-            node.failureLinks.set(EMPTY, this.root)
-        })*/
-    }
-    
-    private addWord(word: string, prevNode: TrieNode, index: number) {
-        if (index >= word.length) {
-            return
-        }
-        
-        const char = word[index];
-        const isEnd = index == word.length - 1;
-        let node = prevNode.next.get(char);
-        if (!node) {
-            node = new TrieNode(char, prevNode, isEnd);
-            prevNode.next.set(char, node);
-        }
-        
-        this.addWord(word, node, index + 1);
     }
 }
