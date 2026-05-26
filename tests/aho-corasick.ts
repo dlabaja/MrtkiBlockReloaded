1e8c6210e5e2448af1fdf4d6e02beb91b2c82ce1
dlabaja <dlabaja.jan@seznam.cz>
2026-05-26 14:21:43 +0200

    testy

--- tests/aho-corasick.ts
+++ tests/aho-corasick.ts
@@ -1,7 +1,11 @@
-// přebráno odsud: https://www.youtube.com/watch?v=O7_w001f58c
 import {Trie} from "../src/scripts/aho-corasick/trie";
 
 export function testAhoCorasick() {
+    testTutorialExample();
+}
+
+// přebráno odsud: https://www.youtube.com/watch?v=O7_w001f58c
+function testTutorialExample() {
     const words = ["acc", "atc", "cat", "gcg"]
     const text = "gcatcg";
     const trie = new Trie(words);
