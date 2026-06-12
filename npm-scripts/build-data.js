8538287f312461fd1d10d47d304cb0f80c10df97
dlabaja <dlabaja.jan@seznam.cz>
2026-06-12 14:30:09 +0200

    update schématu (přidán timestamp)

--- npm-scripts/build-data.js
+++ npm-scripts/build-data.js
@@ -11,32 +11,39 @@ const validator = new Validator();
 const matchesAndReplacements = [];
 
 function buildData() {
-    const resultObjects = [];
+    const jsonItems = [];
     fs.readdirSync(itemsPath, {recursive: true}).forEach(name => {
         if (!name.endsWith(".json")) {
             return;
         }
         
         try {
-            processJson(Path.join(itemsPath, name), resultObjects);
+            processJsonItem(Path.join(itemsPath, name), jsonItems);
         }
         catch (e) {
             throw new Error(`Cannot process ${name}`)
         }
     })
     
-    fs.writeFileSync(outputPath, JSON.stringify(resultObjects), {encoding: "utf-8"});
+    fs.writeFileSync(outputPath, generateJson(jsonItems), {encoding: "utf-8"});
     fs.writeFileSync(nameListPath, matchesAndReplacements.map(x => `${x[0]} - ${x[1].join("; ")}`).sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join("\n"), {encoding: "utf-8"})
 }
 
-function processJson(path, resultObjects) {
+function processJsonItem(path, jsonItems) {
     const instance = JSON.parse(fs.readFileSync(path, { encoding: 'utf8', flag: 'r' }));
     const result = validator.validate(instance, schema);
     if (!result.valid) {
         console.error(`${path} is not valid against schema`);
     }
     matchesAndReplacements.push([instance.name, instance.cases["1"].replacements])
-    resultObjects.push(instance)
+    jsonItems.push(instance)
+}
+
+function generateJson(jsonItems) {
+    return JSON.stringify({
+        timestamp: new Date().toISOString(),
+        items: jsonItems
+    })
 }
 
 buildData();
